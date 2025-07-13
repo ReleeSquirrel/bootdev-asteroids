ba293937fdc93c3b1b9d1bcb0ab8f5231d2bcd56
ReleeSquirrel <relee_s@hotmail.com>
2025-07-12 21:01:15 -0400

    Capped display at 60fps

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +17,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        # Cap at 60fps and capture delta time in seconds
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
